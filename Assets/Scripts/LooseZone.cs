40139d3e870dd3562030b51455147107994be285
msalo37 <stepan200291231232@gmail.com>
2021-06-24 13:47:41 +0300

    Add loose zone

---
+++ Assets/Scripts/LooseZone.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LooseZone : MonoBehaviour
+{
+    private void OnCollisionEnter2D(Collision2D collision2D) 
+    {
+        if (collision2D.gameObject.TryGetComponent<Ball>(out Ball ball))
+        {
+            Debug.Log("You loose!");
+            Destroy(ball.gameObject);
+        }
+    }
+}

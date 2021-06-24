7b38c0c30d30227528901586d2ae063c21e262ad
msalo37 <stepan200291231232@gmail.com>
2021-06-24 13:34:19 +0300

    Add Brick

--- Assets/Scripts/Ball.cs
+++ Assets/Scripts/Ball.cs
@@ -5,9 +5,22 @@ using UnityEngine;
 [RequireComponent(typeof(Rigidbody2D))]
 public class Ball : MonoBehaviour
 {
+    [SerializeField]
+    private int damage;
+
     private Rigidbody2D rb;
-    private void Start() {
+
+    private void Start() 
+    {
         rb = GetComponent<Rigidbody2D>();
-        rb.velocity = new Vector2(Random.Range(-5f, 5f), 0);
+        rb.velocity = new Vector2(5f, 5f);
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision2D) 
+    {
+        if (collision2D.gameObject.TryGetComponent<IDamagable>(out IDamagable damagable))
+        {
+            damagable.Damage(damage);
+        }
     }
 }
---
+++ Assets/Scripts/Brick.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Brick : MonoBehaviour, IDamagable
+{
+    [SerializeField]
+    private int health = 1;
+
+    public void Damage(int damage)
+    {
+        health -= damage;
+
+        if (health <= 0)
+            DestroyBrick();        
+    }
+
+    private void DestroyBrick()
+    {
+        Destroy(gameObject);
+    }
+}

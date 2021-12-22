206e385698cf764816dc68901919a9f251b5de53
msalo37 <stepan200291231232@gmail.com>
2021-12-23 00:36:11 +0400

    Bricks have color now

--- Assets/Scripts/Brick.cs
+++ Assets/Scripts/Brick.cs
@@ -1,16 +1,29 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Brick : MonoBehaviour, IDamagable
 {
+    private int maxHealth;
+    private SpriteRenderer _spriteRenderer;
+    
     [SerializeField]
     private int health = 1;
+
+    private void Awake()
+    {
+        maxHealth = health;
+        _spriteRenderer = GetComponent<SpriteRenderer>();
+        _spriteRenderer.color = CalculateColor(health, maxHealth);
+    }
 
     public void Damage(int damage)
     {
         health -= damage;
 
+        _spriteRenderer.color = CalculateColor(health, maxHealth);
+        
         if (health <= 0)
             DestroyBrick();        
     }
@@ -18,5 +31,15 @@ public class Brick : MonoBehaviour, IDamagable
     private void DestroyBrick()
     {
         Destroy(gameObject);
+    }
+    
+    public int GetHealth()
+    {
+        return health;
+    }
+    
+    private Color32 CalculateColor(float current, float max)
+    {
+        return new Color(Mathf.Clamp((1 - current / max), 0, 1), Mathf.Clamp((current / max), 0, 1), 0, 0.5f);;
     }
 }
